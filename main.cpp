9b62d5e6f5e4caf46aca0743724f0fc954cbc245
Wenzhe Zhou <zwzmzd@mail.enjoy-what.com>
2015-09-28 19:43:33 +0800

    注意替换一下pro文件中库所在路径，还有path末尾加入bin目录

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include <opencv2/opencv.hpp>
+
 int main(int argc, char *argv[])
 {
+    cv::Mat b = imread("C:\\Users\\mzd\\Desktop\\a.png");
+    cv::imshow("nihao", b);
+    cv::waitKey(0);
+    return 0;
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
